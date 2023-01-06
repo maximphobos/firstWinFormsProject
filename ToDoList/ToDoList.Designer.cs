808da2c66e94e4603a475bad941809aca20d2f2d
maximphobos <maximphobos@gmail.com>
2023-01-06 18:18:38 +0200

    Implement TODO list functionality

--- ToDoList/ToDoList.Designer.cs
+++ ToDoList/ToDoList.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ToDoList
 {
-    partial class ToDoList
+    partial class ToDoListForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,24 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToDoList));
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToDoListForm));
+            this.taskTextBoxLabel = new System.Windows.Forms.Label();
+            this.textBoxTaskDescription = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.dataGridViewTasks = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteTaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTasks)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // ToDoList
+            // taskTextBoxLabel
+            // 
+            this.taskTextBoxLabel.AutoSize = true;
+            this.taskTextBoxLabel.Location = new System.Drawing.Point(72, 36);
+            this.taskTextBoxLabel.Name = "taskTextBoxLabel";
+            this.taskTextBoxLabel.Size = new System.Drawing.Size(78, 15);
+            this.taskTextBoxLabel.TabIndex = 0;
+            this.taskTextBoxLabel.Text = "Add new task";
+            // 
+            // textBoxTaskDescription
+            // 
+            this.textBoxTaskDescription.AccessibleDescription = "tyjhtyj";
+            this.textBoxTaskDescription.Location = new System.Drawing.Point(156, 33);
+            this.textBoxTaskDescription.MaxLength = 70;
+            this.textBoxTaskDescription.Name = "textBoxTaskDescription";
+            this.textBoxTaskDescription.PlaceholderText = "Type here task description...";
+            this.textBoxTaskDescription.Size = new System.Drawing.Size(511, 23);
+            this.textBoxTaskDescription.TabIndex = 1;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(693, 33);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "ADD";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // dataGridViewTasks
+            // 
+            this.dataGridViewTasks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTasks.Location = new System.Drawing.Point(12, 76);
+            this.dataGridViewTasks.Name = "dataGridViewTasks";
+            this.dataGridViewTasks.ReadOnly = true;
+            this.dataGridViewTasks.RowTemplate.Height = 25;
+            this.dataGridViewTasks.Size = new System.Drawing.Size(776, 362);
+            this.dataGridViewTasks.TabIndex = 3;
+            this.dataGridViewTasks.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridViewTasks_CellMouseUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteTaskToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(133, 26);
+            // 
+            // deleteTaskToolStripMenuItem
+            // 
+            this.deleteTaskToolStripMenuItem.Name = "deleteTaskToolStripMenuItem";
+            this.deleteTaskToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.deleteTaskToolStripMenuItem.Text = "Delete Task";
+            this.deleteTaskToolStripMenuItem.Click += new System.EventHandler(this.deleteTaskToolStripMenuItem_Click);
+            // 
+            // ToDoListForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.CausesValidation = false;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewTasks);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxTaskDescription);
+            this.Controls.Add(this.taskTextBoxLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.Name = "ToDoList";
+            this.Name = "ToDoListForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TODO List";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTasks)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label taskTextBoxLabel;
+        private TextBox textBoxTaskDescription;
+        private Button buttonAdd;
+        private DataGridView dataGridViewTasks;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem deleteTaskToolStripMenuItem;
     }
 }
--- ToDoList/ToDoList.cs
+++ ToDoList/ToDoList.cs
@@ -1,10 +1,123 @@
+using System.Data;
+using System.Data.SqlClient;
+
 namespace ToDoList
 {
-    public partial class ToDoList : Form
+    public partial class ToDoListForm : Form
     {
-        public ToDoList()
+        private int rowId = 0;
+        private int rowIndex = 0;
+
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+
+        public ToDoListForm()
         {
             InitializeComponent();
+            this.Load += ToDoListForm_Load;
+        }
+
+        private void ToDoListForm_Load(object sender, System.EventArgs e)
+        {
+            dataGridViewTasks.RowTemplate.ContextMenuStrip = contextMenuStrip1;
+            dataGridViewTasks.Select();
+            OpenDbConnection();
+            GetAllTasksRecords();
+            CloseDbConnection();
+        }
+
+        private void dataGridViewTasks_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                dataGridViewTasks.Rows[e.RowIndex].Selected = true;
+                rowIndex = e.RowIndex;
+                dataGridViewTasks.CurrentCell = dataGridViewTasks.Rows[e.RowIndex].Cells[0];
+                Int32.TryParse(dataGridViewTasks.CurrentCell.FormattedValue.ToString(), out rowId);
+                contextMenuStrip1.Show(dataGridViewTasks, e.Location);
+                contextMenuStrip1.Show(Cursor.Position);
+            }
+        }
+
+        private void deleteTaskToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!this.dataGridViewTasks.Rows[this.rowIndex].IsNewRow)
+            {
+                this.dataGridViewTasks.Rows.RemoveAt(this.rowIndex);
+
+                OpenDbConnection();
+                RemoveTaskRecord(rowId);
+                CloseDbConnection();
+            }
+        }
+
+        private void dataGridViewTasks_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            var dataGrid = (DataGridView)sender;
+            if (e.Button == MouseButtons.Right && e.RowIndex != -1)
+            {
+                var row = dataGrid.Rows[e.RowIndex];
+                dataGrid.CurrentCell = row.Cells[e.ColumnIndex == -1 ? 1 : e.ColumnIndex];
+                row.Selected = true;
+                dataGrid.Focus();
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxTaskDescription.Text))
+            {
+                OpenDbConnection();
+                AddTaskRecord();
+                GetAllTasksRecords();
+                CloseDbConnection();
+            }
+            else
+            {
+                MessageBox.Show("Task description can't be empty or whitespace");
+                textBoxTaskDescription.Focus();
+            }
+        }
+
+        private void OpenDbConnection()
+        {
+            string myDbPathOnDisk = @"D:\HillelItSchool\Homeworks\5-TODOlist\ToDoList\";
+            cn = new SqlConnection($@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={myDbPathOnDisk}Database.mdf;Integrated Security=True");
+            cn.Open();
+        }
+
+        private void CloseDbConnection()
+        {
+            cn.Close();
+        }
+
+        private void GetAllTasksRecords()
+        {
+            cmd = new SqlCommand("Select * from Tasks", cn);
+            da = new SqlDataAdapter(cmd);
+            var dt = new DataTable();
+            da.Fill(dt);
+            dataGridViewTasks.DataSource = dt;
+            var column = dataGridViewTasks.Columns[1];
+            column.Width = 610;
+        }
+
+        private void AddTaskRecord()
+        {
+            cmd = new SqlCommand("Insert into Tasks (TaskDescription) Values (@TaskDescription)", cn);
+            cmd.Parameters.AddWithValue("@TaskDescription", textBoxTaskDescription.Text.Trim());
+            cmd.ExecuteNonQuery();
+            textBoxTaskDescription.Text = string.Empty;
+        }
+
+        private void RemoveTaskRecord(int id)
+        {
+            cmd = new SqlCommand("Delete from Tasks where Id = @Id", cn);
+            cmd.Parameters.AddWithValue("@Id", id);
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show($"Task with the Id={id} was removed successfully");
         }
     }
 }
